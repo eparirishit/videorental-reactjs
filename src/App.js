d53c0ddfb213d786e3ec7ce8ee3274890d38b201
Rishit Epari <epari.rishit@gmail.com>
2019-03-28 00:55:21 +0530

    Adding the movies.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Movies from './components/movies';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        Hello World
+        <Movies />
       </main>
     );
   }
